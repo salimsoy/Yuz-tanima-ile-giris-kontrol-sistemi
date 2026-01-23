3c07b4c85495f70dfb0d640de3ba40b55182d63b
salim soy <salimsoy0320@gmail.com>
2026-01-23 15:13:33 +0300

    gerekli kod düzeltmesi yapıldı yorum satırları eklendi

--- facial_recognition.py
+++ facial_recognition.py
@@ -13,12 +13,12 @@ class FacialRecognition:
     
         self.encodings_path = 'faces'
         
-        # Eğer 'faces' klasörü yoksa oluşturur.
+        # Eğer faces klasörü yoksa oluşturur.
         if not os.path.exists(self.encodings_path):
             os.makedirs(self.encodings_path)
             print(f"Uyarı: '{self.encodings_path}' klasörü bulunamadı, yeni oluşturuldu.")
         
-        # Yükleme işlemini yap
+        # Klasördeki kayıtlı yüzlerin Yükleme işlemini yapar
         self.known_encodings, self.class_names = self.load_encodings(self.encodings_path)
         
         print(f"Sistem Başlatıldı Yüklenen kişi sayısı: {len(self.class_names)}")
@@ -32,10 +32,13 @@ class FacialRecognition:
         if not os.path.exists(encodings_path):
             print(f"Hata: '{encodings_path}' klasörü bulunamadı.")
             return [], []
-            
+       
+        # Klasördeki tüm dosyaları tek tek gezer
         for file in os.listdir(encodings_path): 
+            # Sadece .npy uzantılı (NumPy Array) dosyaları alır
             if file.endswith(".npy"): 
                 class_name = file.split('_')[0] 
+                # Matematiksel yüz verisini yükler
                 encoding = np.load(os.path.join(encodings_path, file)) 
                 encodings.append(encoding) 
                 class_names.append(class_name) 
@@ -44,8 +47,9 @@ class FacialRecognition:
     
     def detect_face(self, frame):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
+        # Yüzün kare içindeki koordinatlarını bulur
         face_locations = face_recognition.face_locations(rgb_frame)
+        # Yüzün matematiksel modelini (encoding) çıkarır
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
         
         return face_locations, face_encodings
@@ -57,7 +61,7 @@ class FacialRecognition:
         
         # Eğer veritabanında en az bir kişi varsa işlem yap
         if len(face_distances) > 0:
-           
+            # En küçük mesafeye sahip indeksi bulur
             best_match_index = np.argmin(face_distances)
             min_distance = face_distances[best_match_index]
    
@@ -66,6 +70,7 @@ class FacialRecognition:
         
     def face_drawing(self, frame, name, y1, x2, y2, x1):
         name = name.upper()
+        # Yüzün etrafına dikdörtgen çizer ve 
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) 
         cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED) 
         cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 1)
@@ -74,10 +79,11 @@ class FacialRecognition:
 
     def add_record_log(self, name):
         filename = 'access_log.csv'
+        # Dosya yoksa başlık satırı açmak veya oluşturmak için kontrol eder
         if not os.path.exists(filename):
             with open(filename, 'w') as f: pass
 
-        with open(filename, "a", newline="") as f: # Ekleme modunda
+        with open(filename, "a", newline="") as f: # Ekleme modunda açar
         
             now = datetime.now()
             # Saat formatı
@@ -86,7 +92,7 @@ class FacialRecognition:
             dateString = now.strftime('%Y.%m.%d') 
             # Tarih ve Saati birleştirme
             date_marge = dateString + " " + dtString
-            
+            # Veriyi yazar
             writer = csv.writer(f)
             writer.writerow([date_marge, name])
             print(f"KAYIT EKLENDİ: {name}")
@@ -105,8 +111,9 @@ class FacialRecognition:
                     print("HATA: Kamera açılamadı")
                     break
                 
+                # Görüntüdeki yüzleri analiz et
                 face_locations, face_encodings = self.detect_face(frame)
-                
+                # Ekranda 1 Yüz Varsa çalıştır yoksa uyarı ver
                 if len(face_locations) == 1:
                     face_encoding = face_encodings[0]
                     face_location = face_locations[0]
@@ -122,13 +129,15 @@ class FacialRecognition:
                     else:
                         print("Erişim reddedildi")
                     
+                    # CSV Dosyasına Kaydeder
                     self.add_record_log(name)
-                    
+                    # Ekrana Çizim Yap
                     y1, x2, y2, x1 = face_location 
                     frame = self.face_drawing(frame, name, y1, x2, y2, x1)
                     
                     cv2.imshow("Kamera", frame)
                     cv2.waitKey(0)
+                    
                     return
                 
                 elif len(face_locations) == 0:
@@ -138,11 +147,13 @@ class FacialRecognition:
                     cv2.putText(frame, "Birden Fazla Yuz Var", (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 1)
                 
                 cv2.imshow("Kamera", frame)       
-              
+                
+                # 'q' tuşuna basılırsa manuel çıkış yap
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             
         except Exception as e:
+            # Beklenmedik bir hata olursa program çökmesin, hatayı yazar
             print(f"HATA: {e}")
                 
         finally:
